5c6867402f97e839aebd8e402bdf5175ba98cfec
Aline <36476341+alineborak@users.noreply.github.com>
2023-04-24 12:12:22 +0200

    Last changes

--- page-objects/ConfirmationOrderPage.js
+++ page-objects/ConfirmationOrderPage.js
@@ -5,25 +5,26 @@ class ConfirmationOrderPage {
         this.orderConfirmation = page.locator('h1[class="hero-primary"]');
         this.viewMyOrders = page.locator('button[routerlink="/dashboard/myorders"]');
         this.body = page.locator('tbody');
-        this.orderId = page.locator('.em-spacer-1 .ng-star-inserted');
-        this.orderIdField = page.locator('.col-text').textContent();
+        this.orderIdAtConfirmation = page.locator('.em-spacer-1 .ng-star-inserted');
+        this.orderIdField = page.locator('.col-text');
         this.page = page;
     }
 
     async getOrderConfirmation() {
         await this.orderConfirmation.textContent('Thankyou for the order.');
         await this.viewMyOrders.click();
-        const getId = await this.orderId.textContent();
+        const getId = await this.orderIdAtConfirmation.textContent();
         console.log(getId);
         await this.body.waitFor();
     }
 
     async checkOrderId() {
-        const rows = await this.page.locator('tbody tr');
-        for (let i = 0; i < await rows.count(); ++i) {
-            const rowOrderId = await rows.nth(i).locator("th").textContent();
-            if (this.orderId.includes(rowOrderId)) {
-                await rows.nth(i).locator("button").first().click();
+        const orderId = await this.orderIdAtConfirmation.textContent();
+        const rowsWithOrders = await this.page.locator('tbody tr');
+        for (let i = 0; i < await rowsWithOrders.count(); ++i) {
+            const rowOrderId = await rowsWithOrders.nth(i).locator("th").textContent();
+            if (orderId.includes(rowOrderId)) {
+                await rowsWithOrders.nth(i).locator("button").first().click();
                 break;
             }
         }
--- tests/exercise.spec.js
+++ tests/exercise.spec.js
@@ -32,37 +32,33 @@ test.only('Login and making an order', async ({ page }) => {
     // await orderPage.verifyEmailId();
     await orderPage.clientOrderInput();
 
+
+    // POM IMPLEMENTATION - NOT WORKING
     // Getting Order ID Number and asserting on My Orders page
-    // const confirmationOrderPage = new ConfirmationOrderPage(page);
+    const confirmationOrderPage = new ConfirmationOrderPage(page);
     // const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
-    // await confirmationOrderPage.getOrderConfirmation();
-    // await confirmationOrderPage.checkOrderId();
+    await confirmationOrderPage.getOrderConfirmation();
+    await confirmationOrderPage.checkOrderId();
 
-    // const rows = await page.locator('tbody tr');
-    // for (let i = 0; i < await rows.count(); ++i) {
-    //     const rowOrderId = await rows.nth(i).locator("th").textContent();
-    //     if (orderId.includes(rowOrderId)) {
-    //         await rows.nth(i).locator("button").first().click();
-    //         break;
-    //     }
-    // }
 
+     // ORIGINAL WORKING TEST - WITHOUT POM:
      // Getting Order ID Number and asserting on My Orders page
-     const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
-     console.log(orderId);
-     await page.locator('button[routerlink="/dashboard/myorders"]').click();
-     await page.locator('tbody').waitFor();
- 
-     const rows = await page.locator('tbody tr');
-     for (let i = 0; i < await rows.count(); ++i) {
-         const rowOrderId = await rows.nth(i).locator("th").textContent();
-         if (orderId.includes(rowOrderId)) {
-             await rows.nth(i).locator("button").first().click();
-             break;
-         }
-     }
-     // Check if order has been finalized
-     const orderIdDetails = await page.locator('.col-text').textContent();
-     console.log(orderIdDetails);
-     expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    //  const orderId = await page.locator('.em-spacer-1 .ng-star-inserted').textContent();
+    //  console.log(orderId);
+    //  await page.locator('button[routerlink="/dashboard/myorders"]').click();
+    //  await page.locator('tbody').waitFor();
+
+    //  const rows = await page.locator('tbody tr');
+    //  for (let i = 0; i < await rows.count(); ++i) {
+    //      const rowOrderId = await rows.nth(i).locator("th").textContent();
+    //      if (orderId.includes(rowOrderId)) {
+    //          await rows.nth(i).locator("button").first().click();
+    //          break;
+    //      }
+    //  }
+
+    // Uncomment the test below after previous test is fixed: 
+    // Check if order has been finalized
+    // const orderIdDetails = await page.locator('.col-text').textContent();
+    // expect(orderId.includes(orderIdDetails)).toBeTruthy();
 });
